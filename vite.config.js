814144289fa0b85298bf8003b372b04e0d41d05a
Jodan Elysee <jodanelysee@gmail.com>
2024-10-08 17:05:00 -0400

    allowing vite to listen on all ports

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // Use Render's assigned port or fallback to 3000 for local dev
+    host: true,  // Listen on all IPs (0.0.0.0) so Render can access the app
+  },
 })
